f92fc09c0ef98da98dc4a74c5b8141f0ae39e8a6
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-05 14:54:02 -0300

    feat: create model e migration File and Job

---
+++ api/app-laravel/app/Models/Job.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'site',
+        'page',
+        'format',
+        'other_formats',
+        'phrase',
+        'content',
+        'obs',
+        'type',
+        'status',
+        'user_id',
+    ];
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ api/app-laravel/database/migrations/2024_03_29_020943_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->string('site')->nullable();
+            $table->string('page')->nullable();
+            $table->string('format')->nullable();
+            $table->string('other_formats')->nullable();
+            $table->string('phrase')->nullable();
+            $table->text('content')->nullable();
+            $table->text('obs')->nullable();
+            $table->string('type')->nullable(); //Atualizações - Mídia Digital - Impresso
+            $table->enum('status', ['pending', 'approving', 'changing', 'approved'])->default('pending');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
