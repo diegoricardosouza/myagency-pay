a7a3e325b117cc17bc25710f403cd439185d781a
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-06 18:32:56 -0300

    feat: install and config frontend

---
+++ front/src/Router/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<h1>Login</h1>} />
+
+        <Route path="/" element={<h1>Dashboard</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
