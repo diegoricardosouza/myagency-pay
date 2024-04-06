31035355eaf113dce505a6dca7a13c7822ec3572
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-06 16:34:15 -0300

    feat: create routes api

--- api/app-laravel/routes/api.php
+++ api/app-laravel/routes/api.php
@@ -1,28 +1,29 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\CommentController;
+use App\Http\Controllers\Api\v1\FileCommentController;
+use App\Http\Controllers\Api\v1\FileController;
+use App\Http\Controllers\Api\v1\JobController;
 use App\Http\Controllers\Api\v1\PlanController;
 use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::prefix('v1')->group(function() {
     Route::post('/login', [AuthController::class, 'auth']);
-    // Route::get('/users', [UserController::class, 'index']);
-    // Route::get('/plans', [PlanController::class, 'index']);
-    // Route::post('/plans', [PlanController::class, 'store']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
         Route::apiResource('/plans', PlanController::class);
         Route::apiResource('/users', UserController::class);
+        Route::apiResource('/jobs', JobController::class);
+        Route::apiResource('/files', FileController::class);
+        Route::apiResource('/comments', CommentController::class);
+        Route::apiResource('/files-comments', FileCommentController::class);
     });
-
-
 
     // Route::post('/users/{id}', [UserController::class, 'update']);
 });
 
+Route::get('/', fn () => response()->json(['message' => 'ok']));
