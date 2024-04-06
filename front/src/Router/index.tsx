27ffd73e509b09b34c0efb846c7a57e33d92f836
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-06 20:45:10 -0300

    feat: create login page

--- front/src/Router/index.tsx
+++ front/src/Router/index.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "../view/pages/Dashboard";
+import { Login } from "../view/pages/Login";
+import { AuthGuard } from "./AuthGuard";
+
+
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<h1>Login</h1>} />
 
-        <Route path="/" element={<h1>Dashboard</h1>} />
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route element={<AuthGuard isPrivate />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
