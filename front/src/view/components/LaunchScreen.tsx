1f7ec4d81d1b3d7066771ffa3af3efd5928c2df8
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-08 18:00:22 -0300

    feat: create component LaunchScreen

---
+++ front/src/view/components/LaunchScreen.tsx
@@ -0,0 +1,26 @@
+import { Transition } from "@headlessui/react";
+import { Spinner } from "./Spinner";
+
+interface LaunchScreenProps {
+  isLoading: boolean;
+}
+
+export function LaunchScreen({ isLoading }: LaunchScreenProps) {
+  return (
+    <Transition
+      show={isLoading}
+      enter="transition-opacity duration-75"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-150"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="bg-primary fixed top-0 left-0 w-full h-full grid place-items-center">
+        <div className="flex flex-col items-center gap-4">
+          <Spinner className="text-primary fill-white" />
+        </div>
+      </div>
+    </Transition>
+  )
+}
