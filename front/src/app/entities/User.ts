355c71b69b851b680421d8e450f83cec2b5c7636
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-08 20:59:36 -0300

    fix: fix types User

--- front/src/app/entities/User.ts
+++ front/src/app/entities/User.ts
@@ -1,19 +1,21 @@
 export interface User {
-  id: string;
-  name: string;
-  company: string;
-  responsible: string;
-  email: string;
-  level: string;
-  whatsapp: string;
-  day: number;
-  logo: string;
-  plan: {
+  data: {
     id: string;
     name: string;
-    updates: string;
-    digital_midia: string;
-    printed: string;
-    presentations: string;
+    company: string;
+    responsible: string;
+    email: string;
+    level: string;
+    whatsapp: string;
+    day: number;
+    logo: string;
+    plan: {
+      id: string;
+      name: string;
+      updates: string;
+      digital_midia: string;
+      printed: string;
+      presentations: string;
+    }
   }
 }
