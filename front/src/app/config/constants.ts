def1874c396ffcc5571e30671ffdbf96f55ea9f7
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-09 23:25:38 -0300

    feat: create utils constants

---
+++ front/src/app/config/constants.ts
@@ -0,0 +1,28 @@
+export interface LevelProps {
+  value: string;
+  label: string;
+}
+
+export const LEVELS = [
+  {
+    value: "ADMIN",
+    label: "Admin"
+  },
+  {
+    value: "EDITOR",
+    label: "Editor"
+  },
+  {
+    value: "CLIENTE",
+    label: "Cliente"
+  }
+];
+
+export const MAX_FILE_SIZE = 1024 * 1024 * 3;
+
+export const ACCEPTED_IMAGE_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
