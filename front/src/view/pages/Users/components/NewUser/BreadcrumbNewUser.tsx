72cad71f0c1c8b545453e95639f18ab595fc9c42
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-10 22:23:32 -0300

    feat: create component BreadcrumbNewUser

---
+++ front/src/view/pages/Users/components/NewUser/BreadcrumbNewUser.tsx
@@ -0,0 +1,26 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/view/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
+
+export function BreadcrumbNewUser() {
+  return (
+    <Breadcrumb className="hidden md:flex">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/">Dashboard</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/usuarios">Usuários</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Novo Usuário</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
