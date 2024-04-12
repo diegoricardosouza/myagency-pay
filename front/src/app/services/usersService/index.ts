3d3623a1f216f92acc826f8a506c40dd54939f35
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-11 23:12:09 -0300

    feat: add service getById and update in the index

--- front/src/app/services/usersService/index.ts
+++ front/src/app/services/usersService/index.ts
@@ -1,11 +1,15 @@
 import { create } from "./create";
 import { getAll } from "./getAll";
+import { getById } from "./getById";
 import { me } from "./me";
 import { remove } from "./remove";
+import { update } from "./update";
 
 export const usersService = {
   me,
   create,
   getAll,
-  remove
+  remove,
+  getById,
+  update
 }
