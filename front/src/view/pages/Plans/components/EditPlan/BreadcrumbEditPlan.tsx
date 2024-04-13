ec91bf5004d4b94e0ef20e85864801364423f0f2
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-12 22:18:28 -0300

    feat: create component BreadcrumbEditPlan

---
+++ front/src/view/pages/Plans/components/EditPlan/BreadcrumbEditPlan.tsx
@@ -0,0 +1,26 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/view/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
+
+export function BreadcrumbEditPlan() {
+  return (
+    <Breadcrumb className="hidden md:flex">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/">Dashboard</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/planos">Planos</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Editar Plano</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
