5bac5aba97420340bd4644466cb42e43e08b493e
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-17 09:58:00 -0300

    feat: create constants digital midia and printed

--- front/src/app/config/constants.ts
+++ front/src/app/config/constants.ts
@@ -18,6 +18,40 @@ export const LEVELS = [
   }
 ];
 
+export const FORMATS_DIGITAL_MIDIA = [
+  {
+    value: "Feed",
+    label: "Feed"
+  },
+  {
+    value: "Storie",
+    label: "Storie"
+  },
+  {
+    value: "Telão",
+    label: "Telão"
+  }
+];
+
+export const FORMATS_PRINTED = [
+  {
+    value: "A3 (29,7x42cm)",
+    label: "A3 (29,7x42cm)"
+  },
+  {
+    value: "A4 (21x29,7cm)",
+    label: "A4 (21x29,7cm)"
+  },
+  {
+    value: "Flyer A5 (15x21cm)",
+    label: "Flyer A5 (15x21cm)"
+  },
+  {
+    value: "Cartão de Visita",
+    label: "Cartão de Visita"
+  }
+];
+
 export const MAX_FILE_SIZE = 1024 * 1024 * 3;
 
 export const ACCEPTED_IMAGE_MIME_TYPES = [
