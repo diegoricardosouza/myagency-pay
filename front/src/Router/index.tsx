3fda8332c66131f69f1077a458976c4c44242997
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-17 23:18:23 -0300

    feat: create routes users, plans and jobs

--- front/src/Router/index.tsx
+++ front/src/Router/index.tsx
@@ -1,5 +1,14 @@
 import { DashboardLayout } from "@/view/layouts/DashboardLayout";
 import { Help } from "@/view/pages/Help";
+import { Jobs } from "@/view/pages/Jobs";
+import { FormatsJob } from "@/view/pages/Jobs/components/Formats";
+import { NewFormats } from "@/view/pages/Jobs/components/Formats/NewFormats";
+import Plans from "@/view/pages/Plans";
+import { EditPlan } from "@/view/pages/Plans/components/EditPlan";
+import { NewPlan } from "@/view/pages/Plans/components/NewPlan";
+import User from "@/view/pages/Users";
+import { EditUser } from "@/view/pages/Users/components/EditUser";
+import { NewUser } from "@/view/pages/Users/components/NewUser";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard } from "../view/pages/Dashboard";
 import { Login } from "../view/pages/Login";
@@ -19,6 +28,15 @@ export function Router() {
         <Route element={<AuthGuard isPrivate />}>
           <Route element={<DashboardLayout />}>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/usuarios" element={<User />} />
+            <Route path="/usuarios/novo" element={<NewUser />} />
+            <Route path="/usuarios/edit/:id" element={<EditUser />} />
+            <Route path="/planos" element={<Plans />} />
+            <Route path="/planos/novo" element={<NewPlan />} />
+            <Route path="/planos/edit/:id" element={<EditPlan />} />
+            <Route path="/solicitacoes" element={<Jobs />} />
+            <Route path="/solicitacoes/novo" element={<FormatsJob />} />
+            <Route path="/solicitacoes/:formats" element={<NewFormats />} />
             <Route path="/ajuda" element={<Help />} />
           </Route>
         </Route>
