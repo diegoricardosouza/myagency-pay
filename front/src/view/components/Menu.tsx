0b17f92edb8c753f03e0071849eb54b9cf677a4e
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-17 23:17:57 -0300

    feat: add validations in the component Menu

--- front/src/view/components/Menu.tsx
+++ front/src/view/components/Menu.tsx
@@ -1,6 +1,9 @@
+import { useAuth } from "@/app/hooks/useAuth";
 import { Link } from "react-router-dom";
 
 export function Menu() {
+  const { user } = useAuth();
+
   return (
     <>
       <Link
@@ -9,12 +12,38 @@ export function Menu() {
       >
         Dashboard
       </Link>
+
+      {user?.data.level === 'ADMIN' && (
+        <>
+          <Link
+            to="/usuarios"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Usuários
+          </Link>
+
+          <Link
+            to="/planos"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Planos
+          </Link>
+        </>
+      )}
+
       <Link
-        to="#"
+        to="/solicitacoes"
         className="text-muted-foreground transition-colors hover:text-foreground"
       >
-        Minhas Solicitações
+        {user?.data.level !== 'CLIENTE' && (
+          <span>Solicitações</span>
+        )}
+
+        {(user?.data.level === 'CLIENTE') && (
+          <span>Minhas Solicitações</span>
+        )}
       </Link>
+
       <Link
         to="/ajuda"
         className="text-muted-foreground transition-colors hover:text-foreground"
