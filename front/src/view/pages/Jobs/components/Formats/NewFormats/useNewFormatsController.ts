bcb41392c0184c7db70a92b00d2c52d99061f2a1
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-17 23:04:57 -0300

    feat: create controller useNewFormatsController

---
+++ front/src/view/pages/Jobs/components/Formats/NewFormats/useNewFormatsController.ts
@@ -0,0 +1,84 @@
+import { jobsService } from "@/app/services/jobs";
+import { JobParams } from "@/app/services/jobs/create";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const schema = z.object({
+  site: z.string()
+    .optional()
+    .nullable(),
+  page: z.string()
+    .optional()
+    .nullable(),
+  format: z.string()
+    .optional()
+    .nullable(),
+  other_formats: z.string()
+    .optional()
+    .nullable(),
+  phrase: z.string()
+    .optional()
+    .nullable(),
+  content: z.string()
+    .min(1, 'Conteúdo é obrigatório'),
+  obs: z.string()
+    .optional()
+    .nullable(),
+  files: z.array(z.any()).optional().nullable(),
+});
+
+type FormData = z.infer<typeof schema>
+
+export function useNewFormatsController() {
+  const { formats } = useParams();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    control,
+    reset,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data: JobParams) => {
+      return jobsService.create(data);
+    }
+  });
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      console.log('ok', data);
+
+      await mutateAsync({
+        ...data,
+        type: formats === 'atualizacoes' ? 'Atualizações' : (formats === 'midia-digital' ? 'Mídia Digital' : 'Impresso')
+      });
+
+      queryClient.invalidateQueries({ queryKey: ['jobs'] });
+      toast.success('Solicitação cadastrada com sucesso!');
+      reset();
+      navigate("/solicitacoes");
+    } catch (error) {
+      toast.error('Erro ao cadastrar a solicitação');
+    }
+  });
+
+  return {
+    register,
+    control,
+    reset,
+    errors,
+    handleSubmit,
+    formats,
+    isPending
+  }
+}
