9cb414cfae69ced857bab5dd3cc1ab850dbfa75f
Diego Ricardo <diegoricardoweb@gmail.com>
2024-04-26 20:41:15 -0300

    feat: add content in the page Dashboard

--- front/src/view/pages/Dashboard/index.tsx
+++ front/src/view/pages/Dashboard/index.tsx
@@ -1,24 +1,157 @@
-import { BreadcrumbDashboard } from "./components/BreadcrumbDashboard";
-import { FormatsDashboard } from "./components/Formats";
+import { Search } from "@/view/components/Search";
+import { Spinner } from "@/view/components/Spinner";
+import { Button } from "@/view/components/ui/button";
+import { ScrollArea } from "@/view/components/ui/scroll-area";
+import { PlusCircle } from "lucide-react";
+import { Link } from "react-router-dom";
+import { ItemJobCard } from "./components/ItemJobCard";
 import { useDashboardController } from "./useDashboardController";
 
 
 export function Dashboard() {
-  const { user } = useDashboardController();
+  const {
+    user,
+    jobsPending,
+    jobsApproving,
+    jobsChanging,
+    jobsApproved,
+    isLoading,
+    control,
+    dateEndCut,
+    dateStartCut,
+    handleSubmit
+  } = useDashboardController();
 
   return (
     <>
-      <BreadcrumbDashboard />
+      {/* <BreadcrumbDashboard /> */}
 
-      {user?.data.level === 'CLIENTE' && (
-        <FormatsDashboard />
-      )}
-
-      {user?.data.level !== 'CLIENTE' && (
-        <div>
-          <p>Dashboard Admin</p>
+      <div>
+        <div className="flex mb-4">
+          <Button size="sm" className="h-8 gap-1" asChild>
+            <Link to="/solicitacoes/novo">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Nova Solicitação
+              </span>
+            </Link>
+          </Button>
         </div>
-      )}
+
+        <Search
+          isLoading={isLoading}
+          control={control}
+          dateEndCut={dateEndCut}
+          dateStartCut={dateStartCut}
+          handleSubmit={handleSubmit}
+          day={user?.data.day}
+        />
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <h2 className="font-bold text-xl mb-2">Pendentes</h2>
+            <ScrollArea className="h-[500px] w-full rounded-md border p-3 bg-white relative">
+              {isLoading && (
+                <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 bg-white z-[999]">
+                  <Spinner className="w-6 h-6 fill-primary" />
+                </div>
+              )}
+              {jobsPending?.map((job) => {
+                return (
+                  <ItemJobCard
+                    key={job.id}
+                    id={job.id}
+                    name={job.user.company}
+                    dataCreated={new Date(job.created)}
+                    formats={job.format}
+                    reference={job.referencia}
+                    user={user!.data}
+                    comments={job.comments}
+                  />
+                )
+              })}
+            </ScrollArea>
+          </div>
+
+          <div>
+            <h2 className="font-bold text-xl mb-2">Em Alteração</h2>
+            <ScrollArea className="h-[500px] w-full rounded-md border p-3 bg-white">
+              {isLoading && (
+                <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 bg-white z-[999]">
+                  <Spinner className="w-6 h-6 fill-primary" />
+                </div>
+              )}
+
+              {jobsChanging?.map((job) => {
+                return (
+                  <ItemJobCard
+                    key={job.id}
+                    id={job.id}
+                    name={job.user.company}
+                    dataCreated={new Date(job.created)}
+                    formats={job.format}
+                    reference={job.referencia}
+                    user={user!.data}
+                    comments={job.comments}
+                  />
+                )
+              })}
+            </ScrollArea>
+          </div>
+
+          <div>
+            <h2 className="font-bold text-xl mb-2">Em Aprovação</h2>
+            <ScrollArea className="h-[500px] w-full rounded-md border p-3 bg-white">
+              {isLoading && (
+                <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 bg-white z-[999]">
+                  <Spinner className="w-6 h-6 fill-primary" />
+                </div>
+              )}
+
+              {jobsApproving?.map((job) => {
+                return (
+                  <ItemJobCard
+                    key={job.id}
+                    id={job.id}
+                    name={job.user.company}
+                    dataCreated={new Date(job.created)}
+                    formats={job.format}
+                    reference={job.referencia}
+                    user={user!.data}
+                    comments={job.comments}
+                  />
+                )
+              })}
+            </ScrollArea>
+          </div>
+
+          <div>
+            <h2 className="font-bold text-xl mb-2">Aprovados</h2>
+            <ScrollArea className="h-[500px] w-full rounded-md border p-3 bg-white">
+              {isLoading && (
+                <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 bg-white z-[999]">
+                  <Spinner className="w-6 h-6 fill-primary" />
+                </div>
+              )}
+
+              {jobsApproved?.map((job) => {
+                return (
+                  <ItemJobCard
+                    key={job.id}
+                    id={job.id}
+                    name={job.user.company}
+                    dataCreated={new Date(job.created)}
+                    formats={job.format}
+                    reference={job.referencia}
+                    user={user!.data}
+                    comments={job.comments}
+                  />
+                )
+              })}
+            </ScrollArea>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- front/src/view/pages/Dashboard/useDashboardController.ts
+++ front/src/view/pages/Dashboard/useDashboardController.ts
@@ -1,9 +1,96 @@
 import { useAuth } from "@/app/hooks/useAuth";
+import { jobsService } from "@/app/services/jobs";
+import { formatedDate } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const schema = z.object({
+  startDate: z.date({
+    required_error: "Data de preenchimento obrigatório.",
+  }),
+  endDate: z.date({
+    required_error: "Data de preenchimento obrigatório.",
+  })
+});
+
+type FormData = z.infer<typeof schema>
 
 export function useDashboardController() {
   const { user } = useAuth();
 
+  // Obtém a data atual
+  const dateNow = new Date();
+  const dateStartCut = (dateNow.getMonth() + 1) + '-' + user?.data.day + '-' + dateNow.getFullYear();
+  const dateEndCut = (dateNow.getMonth() + 2) + '-' + user?.data.day + '-' + dateNow.getFullYear();
+
+  const initStarDate = format(dateStartCut, "yyyy-MM-dd");
+  const initEndDate = format(dateEndCut, "yyyy-MM-dd");
+
+  const [startDate, setStartDate] = useState<string | null | undefined>(initStarDate);
+  const [endDate, setEndDate] = useState<string | null | undefined>(initEndDate);
+  const [, setSearchParams] = useSearchParams();
+
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    control,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+
+  const { data, isFetching, isLoading } = useQuery({
+    queryKey: ['jobs-all', startDate, endDate],
+    queryFn: async () => {
+      const response = await jobsService.getAllNoPagination(startDate, endDate);
+
+      return response;
+    }
+  })
+
+  const jobsPending = data?.data.filter(job => job.status === 'pending');
+  const jobsApproving = data?.data.filter(job => job.status === 'approving');
+  const jobsChanging = data?.data.filter(job => job.status === 'changing');
+  const jobsApproved = data?.data.filter(job => job.status === 'approved');
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      const starDate = formatedDate(data.startDate.toISOString());
+      const endDate = formatedDate(data.endDate.toISOString());
+
+      setStartDate(starDate);
+      setEndDate(endDate);
+
+      setSearchParams(params => {
+        params.set('startDate', String(starDate))
+        params.set('endDate', String(endDate))
+
+        return params;
+      });
+    } catch (error) {
+      toast.error('Erro ao obter os dados!');
+    }
+  });
+
   return {
-    user
+    user,
+    isFetching,
+    jobsPending,
+    jobsApproving,
+    jobsChanging,
+    jobsApproved,
+    register,
+    control,
+    errors,
+    handleSubmit,
+    isLoading,
+    dateStartCut,
+    dateEndCut
   }
 }
