0fd324fdf82b8284b190cb0f92c6635769834fac
Diego Ricardo <diegoricardoweb@gmail.com>
2024-05-02 15:59:51 -0300

    style: change style component HasFile

--- front/src/view/components/HasFile.tsx
+++ front/src/view/components/HasFile.tsx
@@ -13,18 +13,18 @@ export const HasFile = ({ file, removeFile }: HasFileProps) => {
         "w-full h-full rounded-lg transition-all flex items-center"
       )}
     >
-      <div className="bg-muted w-full rounded-md shadow-md flex gap-3 items-center justify-between">
-        <div className="flex flex-1 gap-2 items-center">
-          <div className="w-4 h-4 my-3 ml-4">
-            <FileCheck2 className="w-4 h-4" />
-          </div>
-
-          <span className="text-xs text-gray-500 ml-0">{file?.name}</span>
+      <div className="bg-muted w-full rounded-md shadow-md p-2 h-full">
+        <div className="flex items-center justify-center h-[92px]">
+          <FileCheck2 className="w-12 h-12 m-auto text-primary" />
         </div>
 
-        <button type="button" onClick={removeFile}>
-          <X className="w-4 h-4 mr-4" />
-        </button>
+        <div className="flex items-center justify-between mt-1">
+          <span className="text-xs text-gray-500 ml-0 flex-1">{file?.name}</span>
+
+          <button type="button" onClick={removeFile}>
+            <X className="w-4 h-4" />
+          </button>
+        </div>
       </div>
     </div>
   )
