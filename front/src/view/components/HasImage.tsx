b1f058ef761163aaffb490ef820e5acb84c5a579
Diego Ricardo <diegoricardoweb@gmail.com>
2024-05-02 15:44:31 -0300

    feat: add component HasImage

---
+++ front/src/view/components/HasImage.tsx
@@ -0,0 +1,28 @@
+import { X } from "lucide-react";
+
+interface F extends File {
+  preview: string;
+}
+
+interface HasImageProps {
+  file?: F;
+  removeFile: () => void;
+}
+
+export function HasImage({ file, removeFile }: HasImageProps) {
+  return (
+    <div
+      className="bg-muted w-full rounded-md shadow-md p-2 h-full"
+    >
+      <img src={file?.preview} alt="" className="w-full h-[92px] object-cover" />
+
+      <div className="flex items-center justify-between mt-1">
+        <span className="text-xs text-gray-500 ml-0 flex-1">{file?.name}</span>
+
+        <button type="button" onClick={removeFile}>
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+  )
+}
