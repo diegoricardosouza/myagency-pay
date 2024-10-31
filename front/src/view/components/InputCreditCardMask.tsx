19dfda893e8bbf33d9c563ad6c693e6ee2af34ad
Diego Ricardo <diegoricardoweb@gmail.com>
2024-10-31 14:31:43 -0300

    feat: add component InpuCreditCardMask

---
+++ front/src/view/components/InputCreditCardMask.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils';
+import { MaskedInput, createDynamicNumberMaskGenerator } from 'react-hook-mask';
+
+const mask1 = '9999 9999 9999 9999';
+
+const maskGenerator = createDynamicNumberMaskGenerator(mask1);
+
+interface InputCurrencyMaskProps {
+  value: string;
+  onChange: (value: string) => void;
+  error?: string;
+  className?: string;
+}
+
+export function InputCreditCardMask({ value, onChange, error, className }: InputCurrencyMaskProps) {
+  return (
+    <>
+      <MaskedInput
+        maskGenerator={maskGenerator}
+        value={value}
+        onChange={onChange}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+          error && 'border-red-600'
+        )}
+      />
+
+      {error && (
+        <div className="flex gap-2 items-center text-red-700">
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </>
+  )
+}
