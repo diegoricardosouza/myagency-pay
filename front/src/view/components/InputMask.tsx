04e326ea50b0860afa250b37fb42f08340ea4a51
Diego Ricardo <diegoricardoweb@gmail.com>
2024-11-22 17:18:19 -0300

    feat: add component InputMask

--- front/src/view/components/InputMask.tsx
+++ front/src/view/components/InputMask.tsx
@@ -6,14 +6,15 @@ interface InputMaskProps {
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   error?: string;
+  readOnly?: boolean;
 }
 
-export function InputMask({ mask, value, onChange, error }: InputMaskProps) {
+export function InputMask({ mask, value, onChange, error, readOnly = false }: InputMaskProps) {
   const inputRef = useMask({ mask: mask, replacement: { _: /\d/ }, showMask: true });
 
   return (
     <>
-      <Input ref={inputRef} value={value} onChange={onChange} error={error} />
+      <Input ref={inputRef} value={value} onChange={onChange} error={error} readOnly={readOnly} />
     </>
   );
 }
